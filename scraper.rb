7598c32cb82bedf89b0b4abbe6b42e6e8ce41458
Tony Bowden <tony@tmtm.com>
2019-02-13 08:04:20 +0000

    Simplify the loops
    Go through in one pass, and look back to get the Departement

--- scraper.rb
+++ scraper.rb
@@ -16,35 +16,35 @@ end
 def members_data(url)
   noko = noko_for(url)
 
-  noko.xpath('//h4[contains(.,"partement")]').flat_map do |dep|
-    dep.xpath('following-sibling::table[1]//tr').drop(1).map do |tr|
-      tds = tr.css('td')
+  # table id="table_separate"
+  noko.xpath('//h4[contains(.,"partement")]//following-sibling::table[1]//tr[td]').map do |tr|
+    tds = tr.css('td')
 
-      area_data = tds[1].text.split("\n")
-      #circ = tds[1].text.tidy.sub('unique circ', '1è circ').sub('circ. unique', '1è circ').sub('1ère circ.', '1è circ')
-      circ = area_data[1].to_s.match(/(\d+)è\.?\s*circ\.?\s*d.\s*(.*)/) || []
+    area_data = tds[1].text.split("\n")
+    #circ = tds[1].text.tidy.sub('unique circ', '1è circ').sub('circ. unique', '1è circ').sub('1ère circ.', '1è circ')
+    circ = area_data[1].to_s.match(/(\d+)è\.?\s*circ\.?\s*d.\s*(.*)/) || []
 
-      area = {
-        departement: dep.text.tidy.sub(/DÉPARTEMENT /i, '').sub(/^(du|de l.)\s*/, '').sub(/:$/,''),
-        district:    area_data.first,
-        circ_id:     circ[1],
-        circ:        circ[2],
-      }
-      area[:district] = 'Saint Marc' if area[:district] == 'St. Marc'
-      # TODO handle missing parts
-      area[:id] = 'ocd-division/country:ht/departement:%s/arrondissement:%s/circonscription:%s' %
-        %i[departement district circ_id].map { |i| area[i].to_s.downcase.tr(' ', '_') }
+    dep = tr.xpath('.//preceding::h4').last.text.tidy
+    area = {
+      departement: dep.sub(/DÉPARTEMENT /i, '').sub(/^(du|de l.)\s*/, '').sub(/:$/,''),
+      district:    area_data.first,
+      circ_id:     circ[1],
+      circ:        circ[2],
+    }
+    area[:district] = 'Saint Marc' if area[:district] == 'St. Marc'
+    # TODO handle missing parts
+    area[:id] = 'ocd-division/country:ht/departement:%s/arrondissement:%s/circonscription:%s' %
+      %i[departement district circ_id].map { |i| area[i].to_s.downcase.tr(' ', '_') }
 
-      {
-        name:    tds[0].text.tidy.sub('Siège vacant dû au décès de ', '').sub(/ \(.*?\)/, ''),
-        region:  dep.text.tidy,
-        area_id: area[:id],
-        area:    area[:circ],
-        party:   tds[2].text.tidy,
-        term:    '2015',
-        source:  url,
-      }
-    end
+    {
+      name:    tds[0].text.tidy.sub('Siège vacant dû au décès de ', '').sub(/ \(.*?\)/, ''),
+      region:  dep,
+      area_id: area[:id],
+      area:    area[:circ],
+      party:   tds[2].text.tidy,
+      term:    '2015',
+      source:  url,
+    }
   end
 end
 
